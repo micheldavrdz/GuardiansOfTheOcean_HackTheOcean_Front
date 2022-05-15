5950c566a183fb36fdad7396610d3125173507d3
micheldavrdz <micheldavrdz@gmail.com>
2022-05-14 21:55:07 -0500

    add: navbar and changed to view names

---
+++ src/components/NavBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark">
+    <a class="navbar-brand" href="#">
+      <img
+        src="../assets/img/guardians.png"
+        width="30"
+        height="30"
+        class="d-inline-block align-top"
+        alt="Logo de Guardians of the Ocean"
+      />
+      Guardians of the Ocean</a
+    >
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto ">
+        <li class="nav-item active">
+          <router-link to="/">
+            <a class="nav-link" href="#"
+              >Inicio <span class="sr-only">(current)</span></a
+            >
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link 1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link 2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link 3</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  background: #0d1321;
+}
+</style>
+
+export default {
+  name: 'Navbar',
+  components: {
+  },
+};
--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,16 @@ import HomeView from '../views/HomeView.vue';
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Inicio',
     component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/especies',
+    name: 'especies',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/EspeciesView.vue'),
   },
 ];
 
