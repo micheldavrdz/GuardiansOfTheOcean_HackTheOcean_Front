475a6896238a6219aa689651072a424f56a6fa3a
micheldavrdz <micheldavrdz@gmail.com>
2022-05-15 14:34:17 -0500

    add: Footer

--- src/views/EspeciesView.vue
+++ src/views/EspeciesView.vue
@@ -1,15 +1,21 @@
 <template>
+  <Navbar/>
   <EspeciesList/>
+  <Footer/>
 </template>
 
 <script>
 // @ is an alias to /src
+import Navbar from '@/components/NavBar.vue';
 import EspeciesList from '@/components/Especies/EspeciesList.vue';
+import Footer from '@/components/FooterHome.vue';
 
 export default {
   name: 'EspeciesView',
   components: {
     EspeciesList,
+    Navbar,
+    Footer,
   },
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <Navbar/>
   <ONG/>
+  <Footer/>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from '@/components/NavBar.vue';
 import ONG from '@/components/ONG.vue';
+import Footer from '@/components/FooterHome.vue';
 
 export default {
   name: 'HomeView',
   components: {
     Navbar,
     ONG,
+    Footer,
   },
 };
 </script>
